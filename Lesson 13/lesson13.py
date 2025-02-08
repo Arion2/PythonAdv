c4bc8f5e794d7d3a11f0b426e3b6e9499e5a152d
Arion2 <arion.morina23@gmail.com>
2025-02-08 03:48:20 -0800

    lesson13

---
+++ Lesson 13/lesson13.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+#
+# arr_2d = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+# print(arr_2d)
+#
+# element = arr_2d[1,2]
+# print(element)
+#
+# dimension = arr_2d.ndim
+# print(dimension)
+#
+# arr_size = arr_2d.size
+# print(arr_size)
+
+products = ['apples', 'oranges', 'tangerine', 'pears']
+
+sales = [150, 200, 350, 90]
+
+sales_series = pd.Series(sales, index=products)
+
+total_sales = sales_series.sum()
+
+best_selling_product = sales_series.idxmax()
+
+highest_number = sales_series.max()
+
+print(f"Best Selling product: {best_selling_product} with {highest_number}" )
+
+# print(total_sales)
